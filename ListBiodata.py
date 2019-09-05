65e2f1dad5e0ca4bdb2bf5b9eb38a81802187d5a
evenuss <ucoksilalahi345@gmail.com>
2019-09-05 15:08:47 +0700

    Import Date Time

--- ListBiodata.py
+++ ListBiodata.py
@@ -1,4 +1,13 @@
-#ATJEP RAHMAT
+import datetime
+
+# Contribution by : Evenus
+# Datetime : Thursday 05 September 2019 , 15:05.p.m.
+'''  
+	ADD :
+		- import datetime
+		- retrieve data from (data_diri)
+'''
+
 
 print("Biodata Guru oleh @atjeprahmat")
 print("*"*40)
@@ -15,9 +24,16 @@ print("*"*40)
 data_diri.extend([nama, tempat_lahir, tahun_lahir, kota_asal, sekolah_ngajar, jumlah_siswa])
 print("List Awal : ", data_diri)
 
-umur = 2019-tahun_lahir
+year = datetime.date.today().year
+umur = year-tahun_lahir
 umur_thn_depan = umur + 1
 siswa_thn_depan = jumlah_siswa * 2
 print("*"*40)
-print("Nama saya", nama,".","Saya Lahir di ", tempat_lahir,".","Saya tinggal di ",kota_asal,".","dan Mengajar di", sekolah_ngajar,".","Saya memiliki siswa ", jumlah_siswa,"orang.", "Tahun depan, saya akan berusia", umur_thn_depan, "tahun", "dan akan memiliki siswa sebanyak", siswa_thn_depan, "orang")
+print("Nama saya", data_diri[0],"."
+	,"Saya Lahir di ", data_diri[1]
+	,".","Saya tinggal di ",data_diri[2]
+	,".","dan Mengajar di", data_diri[3]
+	,".","Saya memiliki siswa ", data_diri[4]
+	,"orang.", "Tahun depan, saya akan berusia", umur_thn_depan
+	, "tahun", "dan akan memiliki siswa sebanyak", siswa_thn_depan, "orang")
 print("*"*40)
